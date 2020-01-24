3957f4e38a125a428992b109299824bb48d678ee
jihuayu <jihuayu@users.noreply.github.com>
2020-01-24 00:20:47 +0000

    Automated deployment: Fri Jan 24 00:20:47 UTC 2020 eab916ca9b318f80b971916a934ad5c23d6a1b46

--- index.js
+++ index.js
@@ -40,14 +40,14 @@ async function run() {
             const octokit = new github.GitHub(token);
             let date = new Date();
             date.setUTCHours(0, 0, 0, 0);
-            core.info(`issues from date after ${date.toISOString()}`);
+            core.setOutput(`issues from date after ${date.toISOString()}`);
             let data = (await octokit.issues.listForRepo({
                 owner: owner,
                 repo: repo,
                 per_page: 100,
                 // since: date.toISOString()
             })).data;
-            core.info(data);
+            core.setOutput(data);
             for (let i in data) {
                 let v = data[i];
                 console.log(v)
@@ -59,7 +59,7 @@ async function run() {
                 })).data;
                 v.comments_data = c;
                 let result = template(v);
-                core.info(result);
+                core.setOutput(result);
                 fs.writeFileSync(`${path}/${v.number}.md`, result);
             }
         } else {
